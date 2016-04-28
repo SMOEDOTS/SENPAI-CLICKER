9856fca07cac2f72ba06aaf3d4c3bcb51eb52273
Richard Moe <richmoe07@gmail.com>
2016-04-28 18:28:26 -0500

    BUTTONS
    WOOT WOOT MAJOR FIX!

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var clicks = 0
-function click(){
-	clicks++;
-	if (clicks = 1){
-		document.getElementById("text1").innerHTML = "You have noticed Kohai " + clicks + " time.";
+console.log("All files loaded!")
+var notices = 0;
+function notice(){
+	notices++;
+	console.log("Nya~!")
+	if (notices == 1){
+		document.getElementById("text").innerHTML = "You have noticed Kohai " + notices + " time.";
 	}
 	else{
-		document.getElementById("text1").innerHTML = "You have noticed Kohai " + clicks + " times.";
+		document.getElementById("text").innerHTML = "You have noticed Kohai " + notices + " times.";
 	}
+}
+function upgrade(){
+	console.log("Upgrade clicked")
 }
